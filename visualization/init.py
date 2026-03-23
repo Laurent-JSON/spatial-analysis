964ae79e87a8119b4b4cb2b820e1777208d965db
Laurent Tibasima <121809816+Laurent-JSON@users.noreply.github.com>
2026-03-24 00:07:58 +0200

    Add init.py for geospatial visualization package

---
+++ visualization/init.py
@@ -0,0 +1,8 @@
+"""Geospatial visualization and mapping"""
+
+from .maps import MapVisualizer
+from .plots import StatisticalPlots
+from .animations import MapAnimation
+from .interactive import InteractiveMap
+
+__all__ = ['MapVisualizer', 'StatisticalPlots', 'MapAnimation', 'InteractiveMap']
